d63aa0a7d9adcdefded190a771d853d0eff22207
LautaroReyes <129559110+LautaroReyes@users.noreply.github.com>
2023-06-30 14:12:44 -0300

    CNumber.h

---
+++ CNumber.h
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class CNumber {
+private:
+    int n1;
+    int n2;
+    int n3;
+
+public:
+    CNumber(int, int, int);
+    void max();
+    void min();
+    void prom();
+    void par();
+    void impar();
+};
